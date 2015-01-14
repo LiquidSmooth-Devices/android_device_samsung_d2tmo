90fea86b033299328dc30ff9d784ff6fd0cd2fb9
f3n1xu5 <fenixus@gmail.com>
2015-01-13 20:55:13 -0500

    d2tmo fix for a missing baseband in lollipop
    Changes to 5 files applied based on d2att de-unify commits from CM12 tree.
    Updated baseband fingerprint to match the latest available from androiddeviceinfo
    
    Tested working on unofficial LS-LP-v4.0 builds for liquid_d2tmo.
    
    Change-Id: I414bbfde8ae1df02c01de415b415665652e0c2b9

--- liquid.mk
+++ liquid.mk
@@ -3,14 +3,14 @@ $(call inherit-product, device/samsung/d2tmo/full_d2tmo.mk)
 # Enhanced NFC
 $(call inherit-product, vendor/liquid/config/nfc_enhanced.mk)
 
-# Inherit some common LiquidSmooth stuff.
+# Inherit some common CM stuff.
 $(call inherit-product, vendor/liquid/config/common_phone.mk)
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRODUCT_NAME=d2uc \
+    PRODUCT_NAME=d2tmo \
     TARGET_DEVICE=d2tmo \
-    BUILD_FINGERPRINT="samsung/d2uc/d2att:4.4.2/KOT49H//T999UVDMD5:user/release-keys" \
-    PRIVATE_BUILD_DESC="d2uc-user user 4.4.2 KOT49H T999UVDMD5 release-keys"
+    BUILD_FINGERPRINT="samsung/d2tmo/d2tmo:4.3/JSS15J/T999UVUENC2:user/release-keys" \
+    PRIVATE_BUILD_DESC="d2tmo-user 4.3 JSS15J T999UVUENC2 release-keys"
 
 PRODUCT_NAME := liquid_d2tmo
 PRODUCT_DEVICE := d2tmo
